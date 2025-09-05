fc936367d69fdee4a10ed3065840bead0b14deac
noah.guerin <noah.guerin@student.kdg.be>
2025-09-05 17:39:25 +0200

    Update redirectUri

--- authConfig.ts
+++ authConfig.ts
@@ -2,7 +2,7 @@ export const msalConfig = {
     auth: {
         clientId: "b93a80b4-b045-4220-99ac-f5120c3042d2",
         authority: "https://login.microsoftonline.com/common",
-        redirectUri: "http://localhost:3000",
+        redirectUri: "https://jobapptracker-api-gjbjgfg3exfqfmex.westeurope-01.azurewebsites.net",
     },
     cache: {
         cacheLocation: "localStorage",
