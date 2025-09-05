1994d023a617856b3898d283b26ed99141f08394
noah.guerin <noah.guerin@student.kdg.be>
2025-09-05 18:59:09 +0200

    Change authConfig.ts

--- authConfig.ts
+++ authConfig.ts
@@ -2,7 +2,7 @@ export const msalConfig = {
     auth: {
         clientId: "b93a80b4-b045-4220-99ac-f5120c3042d2",
         authority: "https://login.microsoftonline.com/common",
-        redirectUri: "https://jobapptracker-api-gjbjgfg3exfqfmex.westeurope-01.azurewebsites.net",
+        redirectUri: "https://mango-flower-04c2b6503.1.azurestaticapps.net",
     },
     cache: {
         cacheLocation: "localStorage",
