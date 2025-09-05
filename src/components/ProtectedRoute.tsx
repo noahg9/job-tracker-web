940a3f3d15ed4a244fb4cd86434a5f81e90ce8f2
noah.guerin <noah.guerin@student.kdg.be>
2025-09-05 16:10:09 +0200

    Prepare web app for deployment

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,8 +1,13 @@
+import React from "react";
 import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+interface ProtectedRouteProps {
+    children: React.ReactNode;
+}
+
+const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
     const token = localStorage.getItem("token");
-    return token ? children : <Navigate to="/login" replace />;
+    return token ? <>{children}</> : <Navigate to="/login" replace />;
 };
 
 export default ProtectedRoute;
